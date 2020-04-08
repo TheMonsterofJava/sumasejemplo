574dec2b526a026d054b8a227f4629b93cbb32ee
TheMonsterofjava <osvaldo_lovisolo4500@hotmail.com.ar>
2020-04-08 13:49:14 -0300

    Primera subida de Archivos

---
+++ sumas.java
@@ -0,0 +1,12 @@
+public class sumas {
+
+		public static void main(String[] args){
+
+			System.out.println("Ingrese un numero:");
+			System.out.println("===============\n");
+			
+			
+			
+		}	
+
+}
